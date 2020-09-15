4cbf0d3086444feb397dbe16b0d8de6fe7a9cf52
DANIEL ALBERTO ZAPATA SOTO <dzapatso@contratista.epm.co>
2020-09-14 20:19:00 -0500

    Creación de servicio

--- src/app/components/lista-tareas/lista-tareas.component.ts
+++ src/app/components/lista-tareas/lista-tareas.component.ts
@@ -1,32 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service'
+import { Cerveceria } from 'src/app/model/interfaces/Cerveceria';
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-lista-tareas',
   templateUrl: './lista-tareas.component.html',
-  styleUrls: ['./lista-tareas.component.scss']
+  styleUrls: ['./lista-tareas.component.scss'],
 })
 export class ListaTareasComponent implements OnInit {
+  constructor(private _apiServices: ApiService) {}
 
-  constructor(private _apiServices : ApiService) { }
+  cervecerias: Cerveceria[] = [];
 
-  items : any [] = [
+  items: any[] = [
     {
       id: 1,
-      nombre: "Tarea 1",
-      descripcion : "hdsjdhfjdhfjddfgd hsdjfhjdshfjsdf"
+      nombre: 'Tarea 1',
+      descripcion: 'hdsjdhfjdhfjddfgd hsdjfhjdshfjsdf',
     },
     {
       id: 2,
-      nombre: "Tarea 2",
-      descripcion : "jskahfdjksadfjkshjdf"
+      nombre: 'Tarea 2',
+      descripcion: 'jskahfdjksadfjkshjdf',
     },
     {
       id: 3,
-      nombre: "Tarea 3",
-      descripcion : "sdfjksdhfjdshjfhdjkf dfjkshjdf"
-    }
-  ] 
-  ngOnInit(): void {
-  }
+      nombre: 'Tarea 3',
+      descripcion: 'sdfjksdhfjdshjfhdjkf dfjkshjdf',
+    },
+  ];
 
+  ngOnInit(): void {
+    this._apiServices.invocarApi().subscribe((respuesta: Cerveceria []) => {
+      console.log("Respondió: ");
+      console.log(respuesta);
+      this.cervecerias = respuesta;
+    });
+    console.log("Init");
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cerveceria } from 'src/app/model/interfaces/Cerveceria'
 @Injectable({
   providedIn: 'root',
 })
@@ -7,7 +9,7 @@ export class ApiService {
   url_api = 'https://api.openbrewerydb.org/breweries';
   constructor(private _http: HttpClient) {}
 
-  invocarApi() {
-    return this._http.get(this.url_api);
+  invocarApi(): Observable<Cerveceria[]> {
+    return this._http.get<Cerveceria[]>(this.url_api);
   }
 }
